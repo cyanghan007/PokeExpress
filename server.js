ce74af523c4c53cb5731f094c41de5bb53e9f277
cyanghan007 <chan_yang@hotmail.com>
2022-08-01 20:03:12 -0400

    Show view shows req.params.id

--- server.js
+++ server.js
@@ -16,7 +16,11 @@ app.get('/', (req, res) => {
 app.get('/pokemon', (req, res) => {
    let firstLetterCap = pokemon.map(item => item.charAt(0).toUpperCase() + item.substr(1).toLowerCase());
     res.render('Index', {pokemon: pokemon});
-});  
+});
+
+app.get('/pokemon/:id', (req, res) => {
+     res.send(req.params.id);
+ });  
 
 // My port
 app.listen(port,() => {
